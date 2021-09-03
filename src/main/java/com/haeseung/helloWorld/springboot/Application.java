29ccaf2f05101243be1fec69fbc1422874d75b26
haeseung-lee <dlgotmd93@gmail.com>
2021-09-03 18:50:16 +0900

    0903..git push 테스트

--- src/main/java/com/haeseung/helloWorld/springboot/Application.java
+++ src/main/java/com/haeseung/helloWorld/springboot/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         //내장 WAS 실행 -> 톰캣을 설치할 필요가 없음
-        //git push 재배포
+        //git push 재배포2
         SpringApplication.run(Application.class, args);
     }
 }
